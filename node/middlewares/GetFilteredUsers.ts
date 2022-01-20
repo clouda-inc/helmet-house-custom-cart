5bde27c7b130a9d6ad2a3214e2b386d3a366a41e
devin <devin.gallage@clouda.io>
2022-01-20 13:35:45 +0530

    Add changelog file to the app

--- node/middlewares/GetFilteredUsers.ts
+++ node/middlewares/GetFilteredUsers.ts
@@ -14,7 +14,6 @@ export async function getFilteredUsers(ctx: Context, next: () => Promise<any>) {
       where: `userId=${
         orgarnizationalId.find((u) => u)?.orgId
       } AND shippingMethod='Ground'`,
-      // where: `userId=${orgarnizationalId.find((u) => u)?.orgId}`,
       schema: 'mdv1',
     }
   )
